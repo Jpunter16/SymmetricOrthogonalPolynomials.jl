46d746597f4b530fc92dbd5ce3138c67138ae799
Jpunter16 <jorgepp423@gmail.com>
2026-06-29 18:20:34 +0100

    Update Laplacian.jl

--- examples/Laplacian.jl
+++ examples/Laplacian.jl
@@ -1,21 +1,29 @@
 using SymmetricOrthogonalPolynomials, ClassicalOrthogonalPolynomials, CairoMakie
 import CairoMakie:spy!
 
-N=5
+N=20
 
-alltups = vcat((Partition_2_parts(n) for n=0:N))
+alltups = vcat(([part.p for n in 0:N for part in Partition_2_parts(n)]))
 inds_o = findall(isodd,map(sum, alltups)); inds_e = findall(iseven,map(sum, alltups))
 inds_eo = [inds_o; inds_e]
 
-fig = Figure(size=(600,300))
-Axis(fig[1,1]; yreversed=true)
+fig = Figure(size=(800,400))
+Axis(fig[1,1]; yreversed=true, title="Not using symmetry")
 
 Δ=Matrix(getLaplacianS2InvariantBasis(S2Invariant((Ultraspherical(0.5))),N))
 
 spy!(Δ)
 
-Axis(fig[1,2]; yreversed=true)
+Axis(fig[1,2]; yreversed=true, title="Odd even symmetry")
 
 
 spy!(Δ[inds_eo, inds_eo])
+
+
+str= "Sparsity of Laplace operator n=" * string(N) *".png"
+
+
+save(str,fig)
+
+
 fig
